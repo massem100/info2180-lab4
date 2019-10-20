ffe7273ae366a89bafc9eb1aa631780c3b10ccbc
Krystall Messam <massem100@outlook.com>
2019-10-20 18:49:38 -0500

    Exercise 2 and 3 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,26 +1,37 @@
 "use strict";
 
 window.onload = ()=>{
+    // Select New Game Button, set clicker to restart game.
+    var n_Game = document.getElementsByClassName("btn")[0];
+    n_Game.addEventListener("click", () => {window.location.reload(); });
+  
+    //Select game status
+    let status = document.getElementById("status");
+
     // select statements for the board and inner div statements.
-    let  board= document.getElementById("board").children;
+    let board= document.getElementById("board");
     
-    
-    for (var i =0; i< board.length; i++){
-        board[i].setAttribute('class','square');
-    
-   
+    //Loops through div elements, add square styling, hover and click functions to the board
+    for (let square of board.children) {
+        var tracking = 0;
+        square.classList.add('class','square');
+        square.addEventListener("mouseover", function() {this.classList.add("hover");});
+        square.addEventListener("mouseout", function() {this.classList.remove("hover");});
+        square.addEventListener("click", function(){
+            // In this game, Player X always goes first
+            //
+            if (tracking % 2 === 0 && square.innerHTML ===""&& square.innerHTML !== "O"){
+                square.innerHTML= "X";
+                square.classList.add("square", "X");
+                tracking += 1;
+            }else if ( tracking % 2 === 1 && square.innerHTML ==="" && square.innerHTML !=="X"){
+                square.innerHTML = "O";
+                square.classList.add("square", "O");
+                tracking +=1;
+            }
+            
+        } );
+    }
+    }
 
-}
-    board.addEventListener("mouseover", () => {
-        board.classList.add("hover")
-    });
-    board.addEventListener("mouseleave", () => {
-        board.classList.remove("hover")
-    });
- var btnC = document.getElementsByClassName("btn")[0];
- btnC.addEventListener("click", () => { window.location.reload()});
-
-
-
-}
 
