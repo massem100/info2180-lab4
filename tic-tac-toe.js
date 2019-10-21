c02dd3ed200bbb362770644f0ae55f0aadc4ec04
Krystall Messam <massem100@outlook.com>
2019-10-21 09:46:20 -0500

    Exercise 5 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,13 @@
 window.onload = ()=>{
     // Select New Game Button, set clicker to restart game.
     var n_Game = document.getElementsByClassName("btn")[0];
-    n_Game.addEventListener("click", () => {window.location.reload(); });
+    n_Game.addEventListener("click", () => {
+        let status = document.getElementById("status").innerHTML = "Move your mouse over a square and click to play an X or an O.";
+        let board = document.getElementById("board").children;
+        board.innerHTML="";
+        window.location.reload(); 
+    
+    });
   
     
     // select statements for the board and inner div statements.
@@ -27,13 +33,13 @@ window.onload = ()=>{
         square.addEventListener("mouseout", function() {this.classList.remove("hover");});
         square.addEventListener("click", function(){
             // In this game, Player X always goes first
-            //square.innerHTML ==="" took out condition for now
-            if (tracking % 2 === 0 ){
+            if (tracking % 2 === 0 && square.innerHTML === "") {
                 square.innerHTML= "X";
                 square.classList.add("square", "X");
                 tracking += 1;
                 let s = document.getElementById("board").children;
                 // square 0 is actually square 1.. which further shifts the numbers.
+                //Checking if X has won
                 if (s[0].innerHTML === s[1].innerHTML && s[0].innerHTML === s[2].innerHTML && s[1].innerHTML === s[2].innerHTML && s[0].innerHTML !== "") {
                         displayWinMsg("X");
                 }
@@ -60,8 +66,8 @@ window.onload = ()=>{
                 if (s[2].innerHTML === s[5].innerHTML && s[2].innerHTML === s[8].innerHTML && s[5].innerHTML === s[8].innerHTML && s[2].innerHTML !== "") {
                         displayWinMsg("X");
                 }
-                //&& square.innerHTML ==="" took out condition for now
-            }else if ( tracking % 2 === 1 ){
+                // Checking if O has won
+            } else if (tracking % 2 === 1 && square.innerHTML === "") {
                 square.innerHTML = "O";
                 square.classList.add("square", "O");
                 tracking +=1;
